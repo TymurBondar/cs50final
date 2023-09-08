8ef4ed7cd050c9081124dcc4bcaae8b6090c2b41
Tymur Bondar <tymurbondar@outlook.com>
2023-09-08 11:33:46 -0400

    minor css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
 
@@ -7,3 +7,10 @@ def index():
     if request.method == "GET":
         return render_template ("index.html")
 
+@app.route('/register')
+def register():
+    return render_template ("register.html")
+
+@app.route('/login')
+def login():
+    return render_template ("login.html")
