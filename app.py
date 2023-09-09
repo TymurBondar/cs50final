e7a30e92ab29b8e6b1ee231c755926664a205cf7
Tymur Bondar <tymurbondar@outlook.com>
2023-09-09 18:13:12 -0400

    registration logic

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, session, redirect
+import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -9,6 +11,35 @@ def index():
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Connect to SQLite3 database and execute the SELECT
+        with sqlite3.connect('typefaster.db') as con:
+            cur = con.cursor()
+            cur.execute("SELECT username FROM users WHERE username = ?;", (username,))
+            rows = cur.fetchall()
+        
+        if not username or not password or not confirmation:
+            error_statement = "All fields must be filled"
+            return render_template("error.html", error_statement = error_statement)         
+
+        elif len(rows) != 0:
+            error_statement = "Username already exists"
+            return render_template("error.html", error_statement = error_statement)
+
+        elif confirmation != password:
+            error_statement = "Password doesn't match the confirmation"
+            return render_template("error.html", error_statement = error_statement)
+        else:
+            "add username to the database"
+            hash = generate_password_hash(password)
+            cur.execute("INSERT INTO users(username, password_hash) VALUES (?, ?)", (username, hash))
+            print ("success")
+            return render_template("home.html")
+
     return render_template ("register.html")
 
 @app.route('/login', methods=["GET", "POST"])
